11955499676c5a9d8b94341c106beb1c6127806c
jasonlubrano <jalubrano1@gmail.com>
2018-02-17 20:53:15 -0700

    finished up PA2

--- JasonLubrano-CSCI3753-PA2/simple_char_driver.c
+++ JasonLubrano-CSCI3753-PA2/simple_char_driver.c
@@ -5,9 +5,9 @@
 #include <asm/uaccess.h>
 #include <linux/slab.h>
 
-#define BUFF_SIZE 1024
-#define MAJOR_NUMBER 789
-#define DEVICE_NAME "simple_character_device"
+#define BUFF_SIZE 1024 /* size of the buffer */
+#define MAJOR_NUMBER 789 /* because 7 ate 9 */
+#define DEVICE_NAME "simple_character_device" /* name */
 
 static char *device_buff;
 static int open_Count = 0;
@@ -51,14 +51,21 @@ loff_t simple_char_driver_seek(struct file *pfile, loff_t offset, int whence){
 			new_offset = BUFF_SIZE - offset;
 			break;
 	}
-	if(new_offset > BUFF_SIZE){
-		printk(KERN_ALERT "ERROR: cannot seek past BUFF_SIZE");
-	} if(new_offset < 0){
+
+	/* because the lab writeup made me worried about this*/
+	if(new_offset < 0){
 		printk(KERN_ALERT "ERROR: cannot seek before buffer start");
 	}
-	pfile->f_pos = new_offset;
-	return new_offset;
 
+	if(new_offset > BUFF_SIZE){
+		printk(KERN_ALERT "ERROR: cannot seek past BUFF_SIZE");
+	}
+
+	/* make the file position equal to our new offset*/
+	pfile->f_pos = new_offset;
+
+	/*return how many bytes the new offset is */
+	return new_offset;
 }
 
 ssize_t simple_char_driver_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset){
@@ -67,9 +74,8 @@ ssize_t simple_char_driver_read(struct file *pfile, char __user *buffer, size_t 
 	/* max num of bytes that can be read from offset to BUFF_SIZE */
 	int MAX_BYTES = BUFF_SIZE - *offset;
 	/*number of bytes to read */
-	int BYTES_TO_READ;
 	/*number of bytes that we passed through */
-	int BYTES_READ;
+	int BYTES_TO_READ, BYTES_READ;
 	/* logical statements about length of reading buffer */
 	if(MAX_BYTES > length){
 		BYTES_TO_READ = length;
@@ -81,7 +87,7 @@ ssize_t simple_char_driver_read(struct file *pfile, char __user *buffer, size_t 
 		printk(KERN_ALERT "END OF DEVICE \n");
 	}
 	/* how many bytes did we read */
-	BYTES_READ = BYTES_TO_READ - copy_to_user(buffer, device_buff + *offset, BYTES_TO_READ);
+	BYTES_READ = BYTES_TO_READ - copy_to_user(buffer, device_buff + *offset, BYTES_TO_READ);	
 	printk(KERN_ALERT "DEVICE READ: %d BYTES\n", BYTES_READ);
 	/* move the offset hoewver many bytes we read */
 	*offset += BYTES_READ;
@@ -97,9 +103,8 @@ ssize_t simple_char_driver_write(struct file *pfile, const char __user *buffer, 
 	/* max num of bytes that can be WRITE from offset to BUFF_SIZE */
 	int MAX_BYTES = BUFF_SIZE - *offset;
 	/*number of bytes to WRITE */
-	int BYTES_TO_WRITE;
 	/*number of bytes WRITTEN */
-	int BYTES_WROTE;
+	int BYTES_TO_WRITE, BYTES_WROTE;
 	/* logical statements about length of writing and buffer */	
 	if(MAX_BYTES > length){
 		BYTES_TO_WRITE = length;
@@ -123,7 +128,7 @@ ssize_t simple_char_driver_write(struct file *pfile, const char __user *buffer, 
 struct file_operations simple_char_driver_file_operations ={
 	.owner		= THIS_MODULE,					/* in the file op struct */
 	.open 		= simple_char_driver_open, 		/* opens */
-	.release	= simple_char_driver_close, 	/* closes. NO .close, use release instead? */
+	.release	= simple_char_driver_close, 	/* closes*/
 	.llseek 	= simple_char_driver_seek, 		/* seeks */
 	.read 		= simple_char_driver_read, 		/* reads */
 	.write 		= simple_char_driver_write, 	/* writes */
@@ -132,22 +137,21 @@ struct file_operations simple_char_driver_file_operations ={
 int simple_char_driver_init(void){
 	/* copy from helloModule where I am */
 	printk(KERN_ALERT "inside %s function\n",__FUNCTION__);
+	/* free up space before init */
+	device_buff = kmalloc(BUFF_SIZE, GFP_KERNEL);
 	/* register w/ kern and indicate we are doing the char_device_drv*/
 	register_chrdev(MAJOR_NUMBER, DEVICE_NAME, &simple_char_driver_file_operations);
-
-	device_buff = kmalloc(BUFF_SIZE, GFP_KERNEL);
 	return 0;
 }
 
 void simple_char_driver_exit(void){
 	/* copy from helloModule where I am */
 	printk(KERN_ALERT "inside %s function\n",__FUNCTION__);
-
+	if(device_buff){
+		kfree(device_buff);
+	}
 	/* unregister the char dev drv */
 	unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME);
-
-	void kfree(device_buff);
-	return 0;
 }
 
 module_init(simple_char_driver_init);
--- JasonLubrano-CSCI3753-PA2/simple_char_driver_test.c
+++ JasonLubrano-CSCI3753-PA2/simple_char_driver_test.c
@@ -3,40 +3,63 @@
 #include <fcntl.h>
 #include <stdbool.h>
 
-#define DEVICE "/dev/simple_character_device"
+#define DEVICENAME "/dev/simple_character_device"
 #define BUFF_SIZE 1024
 
 int main(){
 	char command;
+	int length, whence, new_offset;
 	char buffer[BUFF_SIZE];
-	int file = open(DEVICE, O_RDWR);
+	int file = open(DEVICENAME, O_RDWR);
 	bool running = true;
 	while(running){
+		printf("\n\nJason's Program\n");
 		printf("COMMANDS:\n");
 		printf("	'r' to read from device\n");
-		printf("	'w' to write from device\n");
+		printf("	'w' to write to device\n");
 		printf("	's' to seek from device\n");
 		printf("	'e' to exit from device\n");
-		printf("	anything else to keep reading/writing\n");
-		printf("$> ");
+		printf("	anything else brings up main menu\n");
+		printf("/SCD/command$> ");
 		scanf("%c", &command);
 
 		switch(command){
-			case 'r': read(file, buffer, BUFF_SIZE);
-				printf("\nr> %s\n", buffer);
-				while(getchar() != '\n');
+			case 'r':
+				printf("/SCD/read$> How many bytes to read: "); /* ask user how many bytes */
+				scanf("%d", &length); /* user inputs length of how many bytes */
+				read(file, buffer, length); /* reads from the file, puts it to the buffer for x-length */
+				printf("/SCD/read$> %s\n", buffer); /* prints the buffer */
+				while(getchar() != '\n'); /* while its not a new line, continue */
 				break;
-			case 'w': printf("\nw> ");
-				gets(buffer);
-				write(file, buffer, BUFF_SIZE);
-				while(getchar() != '\n');
+			case 'w':
+				printf("/SCD/write$> "); /* User writes to the file*/
+				//fgets(buffer, sizeof(buffer), sdtin); /* get the user strings */
+				/* fgets didnt work so I will ahve to do write
+					only problem with that is that it is stopping
+					at the first whitespace. anything else gave me a
+					segmentation fault.*/
+				scanf("%s", buffer);
+				write(file, buffer, BUFF_SIZE); /* writes the buffer to file */
+				while (getchar() != '\n'); /* while its not a new line, continue */
 				break;
-			case 's': printf("\ns>\n");
+			case 's':
+				printf("SEEK COMMANDS:\n");
+				printf("	'0' seek set\n");
+				printf("	'1' seek cur\n");
+				printf("	'2' seek end\n");
+				printf("	anything else brings up main menu\n");
+				printf("/SCD/seek$> Enter whence: ");
+				scanf("%d", &whence);
+				printf("\n/SCD/write$> Enter an offset value: ");
+				scanf("%d", &new_offset);
+				llseek(file, new_offset, whence);
 				break;
-			case 'e': printf("\ne> Exiting\n");
+			case 'e':
+				printf("/SCD/exit$> Exiting\n");
 				running = false;
 				break;
-			default: printf("\n$> error: not a valid command\n");
+			default:
+				printf("\n/SCD/error$> error: not a valid command\n");
 				break;
 		}
 	}
